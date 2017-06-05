b85fd38106bd4b9f0869c00178838d7938915f11
Тарас Протченко <t.protchenko@infotech.team>
2017-06-05 14:29:46 +0300

    edit linter config

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -7,7 +7,7 @@ export const PageLayout = ({ children }) => (
   <div className='container text-center'>
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
+    {` · `}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
     <div className='page-layout__viewport'>
       {children}
